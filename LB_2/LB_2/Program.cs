0db9be76b6c9ea03faf1e2ea94241945ffd8c8c2
Skyba123 <90748947+Skyba123@users.noreply.github.com>
2022-10-05 13:02:21 +0300

    Add files via upload

---
+++ LB_2/LB_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LB_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int nk, nn;
+                bool res = false;
+                double a = 0;
+                do
+                {
+                    Console.WriteLine("nn:");
+                    res = int.TryParse(Console.ReadLine(), out nn);
+                } while (nn <= 0 );
+                res = false;
+                do{
+                    Console.WriteLine("nk:");
+                    res = int.TryParse(Console.ReadLine(), out nk);
+                } while (nk <= nn );
+                for (int k = nn; k < nk; k++){
+                    a += (Math.Pow(-1, k*k+k+1) * Math.Pow(k,2) / (2 * Math.Pow(k, 2)+ 5));
+                }
+                Console.WriteLine("a = {0}", a);
+            }
+        }
+    }
+}
+
