2a1cc4ec85543bd05d1e2074e7ce619833d559e7
Skyba123 <90748947+Skyba123@users.noreply.github.com>
2022-10-05 12:16:57 +0300

    Add files via upload

---
+++ LB__1/LB__1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LB_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, v, g, t;
+            g = 10;
+            t = 0;
+            Console.WriteLine("Введiть висоту(за системою SI):");
+            string H = Console.ReadLine();
+            h = Convert.ToInt32(H);
+            Console.WriteLine("Введiть початкову швидкiсть(за системою SI):");
+            string V = Console.ReadLine();
+            v = Convert.ToInt32(V);
+            t = (-v + Math.Sqrt(Math.Pow(v, 2) + 2 * g * h)) / g;
+            double Hight = v * t + ((g * Math.Pow(t, 2)) / 2);
+            Console.WriteLine("Time:" + t + "с");
+            Console.WriteLine("Hight(перевiрка):" + Hight);
+        }
+    }
+}
+
---
+++ LB__3/LB__3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LB__3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, x1, y1, x2, y2;
+            bool res = true;
+            Console.WriteLine("Введите координаты левой верхней вершины прямоугольника:");
+            Console.WriteLine("x1:");
+            x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("y1:");
+            y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координаты правой нижней вершины прямоугольника:");
+            Console.WriteLine("x2:");
+            x2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("y2:");
+            y2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите коорднаты точки:");
+            Console.WriteLine("x:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("y:");
+            y = int.Parse(Console.ReadLine());
+            if ((x > x1) && (x < x2) && (y < y1) && (y > y2))
+            {
+                res = true;
+                Console.WriteLine(res);
+            } else
+            {
+                res = false;
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
