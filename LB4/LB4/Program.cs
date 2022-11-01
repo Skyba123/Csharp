34c80d06fa80cbcad5cf7ce37647df6d23538cce
Skyba123 <90748947+Skyba123@users.noreply.github.com>
2022-11-01 20:49:09 +0200

    Add files via upload

---
+++ LB4/LB4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LB4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 1, s = 0;
+            Console.Write("Введите размер массива:\t");
+            int N = int.Parse(Console.ReadLine());
+            int[] MyArray = new int[N];
+            Console.WriteLine("Введите значения:");
+            MyArray[0] = int.Parse(Console.ReadLine());
+            for (int i = 1; i < MyArray.Length; i++)
+            {       
+                s = int.Parse(Console.ReadLine());
+                if (MyArray[j - 1] == s )
+                {
+                    continue;
+                }
+                MyArray[j++] = s;
+                Console.WriteLine(s);
+                if (MyArray[j] != MyArray[j - 1]) ;
+            }
+            Console.WriteLine("после изменений : ");
+            for (int i = 0; i < j; i++) Console.Write(MyArray[i] + " ");
+        }
+    }
+}
+
