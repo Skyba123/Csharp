9537e9c34ed6127519f1accc98846ab97d428a6d
Skyba123 <90748947+Skyba123@users.noreply.github.com>
2023-01-08 10:08:34 +0200

    Add files via upload

---
+++ LB5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+namespace LB5
+{
+    class Program
+    {
+        //Дано число k і матриця розміру m *  n . Видалити рядок (стовпчик) матриці з номером k.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк(нажмите enter) и столбцов:\t");
+            int[,] arr = new int[int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())];
+            Random random = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = random.Next(20);
+                }
+            }
+                    for (int i=0;i<arr.GetLength(0);i++)
+            {
+                for (int j=0;j<arr.GetLength(1);j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Выберите число из матрицы для удаления строки");
+            int k = int.Parse(Console.ReadLine());
+            int res = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (k == arr[i, j])
+                    {
+                        res = i;
+                    }
+                }
+            }
+            Console.WriteLine("Результат:");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (i != res)
+                    {
+                        Console.Write(arr[i, j] + "\t");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
