81e8c142e8d4814974f6efeb760f56771bfcc9aa
Serhii Skyba <90748947+Skyba123@users.noreply.github.com>
2023-06-11 03:01:49 +0300

    Add files via upload

---
+++ Курсова/Sea War/Bot.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Sea_War
+{
+    public class Bot
+    {
+        private int[,] myMap;
+        private int[,] enemyMap;
+        private Button[,] myButtons;
+        private Button[,] enemyButtons;
+        private Random random;
+
+        public Bot(int[,] myMap, int[,] enemyMap, Button[,] myButtons, Button[,] enemyButtons)
+        {
+            this.myMap = myMap;
+            this.enemyMap = enemyMap;
+            this.myButtons = myButtons;
+            this.enemyButtons = enemyButtons;
+            random = new Random();
+        }
+
+        private bool IsInsideMap(int i, int j)
+        {
+            return i >= 0 && i < Form1.mapSize && j >= 0 && j < Form1.mapSize;
+        }
+
+        private bool IsEmpty(int i, int j, int length, bool isHorizontal)
+        {
+            if (isHorizontal)
+            {
+                for (int k = j; k < j + length; k++)
+                {
+                    if (!IsInsideMap(i, k) || myMap[i, k] != 0)
+                        return false;
+                }
+            }
+            else
+            {
+                for (int k = i; k < i + length; k++)
+                {
+                    if (!IsInsideMap(k, j) || myMap[k, j] != 0)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsNeighboringCellEmpty(int i, int j)
+        {
+            for (int x = i - 1; x <= i + 1; x++)
+            {
+                for (int y = j - 1; y <= j + 1; y++)
+                {
+                    if (IsInsideMap(x, y) && myMap[x, y] != 0)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public int[,] ConfigureShips()
+        {
+            int[] shipLengths = { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 };
+
+            foreach (int length in shipLengths)
+            {
+                bool isShipPlaced = false;
+
+                while (!isShipPlaced)
+                {
+                    int x = random.Next(0, Form1.mapSize);
+                    int y = random.Next(0, Form1.mapSize);
+                    bool isHorizontal = random.Next(2) == 0;
+
+                    if (isHorizontal)
+                    {
+                        if (IsEmpty(x, y, length, true) && IsNeighboringCellEmpty(x, y - 1) && IsNeighboringCellEmpty(x, y + length))
+                        {
+                            for (int k = y; k < y + length; k++)
+                            {
+                                myMap[x, k] = 1;
+                            }
+
+                            isShipPlaced = true;
+                        }
+                    }
+                    else
+                    {
+                        if (IsEmpty(x, y, length, false) && IsNeighboringCellEmpty(x - 1, y) && IsNeighboringCellEmpty(x + length, y))
+                        {
+                            for (int k = x; k < x + length; k++)
+                            {
+                                myMap[k, y] = 1;
+                            }
+
+                            isShipPlaced = true;
+                        }
+                    }
+                }
+            }
+
+            return myMap;
+        }
+
+        public bool Shoot()
+        {
+            int x, y;
+            bool hit;
+
+            do
+            {
+                x = random.Next(0, Form1.mapSize);
+                y = random.Next(0, Form1.mapSize);
+            } while (enemyButtons[x, y].BackColor == Color.Red || enemyButtons[x, y].BackColor == Color.Blue
+                     || enemyButtons[x, y].BackColor == Color.Gray); 
+
+            if (enemyMap[x, y] != 0)
+            {
+                hit = true;
+                enemyMap[x, y] = 0;
+                enemyButtons[x, y].BackColor = Color.Red;
+                enemyButtons[x, y].Text = "X";
+            }
+            else
+            {
+                hit = false;
+                enemyButtons[x, y].BackColor = Color.Blue;
+            }
+
+            if (hit)
+                Shoot();
+
+            return hit;
+        }
+    }
+}
---
+++ Курсова/Sea War/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sea_War
+{
+    public partial class Form1 : Form
+    {
+        public const int mapSize = 10;
+        public int cellSize = 30;
+        public string alphabet = "ABCDEFGHIJ";
+
+        public int[,] myMap = new int[mapSize, mapSize];
+        public int[,] enemyMap = new int[mapSize, mapSize];
+
+        public Button[,] myButtons = new Button[mapSize, mapSize];
+        public Button[,] enemyButtons = new Button[mapSize, mapSize];
+
+        public bool isPlaying = false;
+        public Label enemyMapLabel;
+        public Bot bot;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Sea War";
+            Init();
+        }
+
+        public void Init()
+        {
+            isPlaying = false;
+            CreateMaps();
+            bot = new Bot(enemyMap, myMap, enemyButtons, myButtons);
+            enemyMap = bot.ConfigureShips();
+
+        }
+        private void ShowWinMessage()
+        {
+            MessageBox.Show("Ви перемогли!", "Перемога", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ShowLoseMessage()
+        {
+            MessageBox.Show("Ви програли!", "Програш", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void CreateMaps()
+        {
+            this.Width = mapSize * 2 * cellSize + 50;
+            this.Height = (mapSize + 3) * cellSize + 20;
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    myMap[i, j] = 0;
+
+                    Button button = new Button();
+                    button.Location = new Point(j * cellSize, i * cellSize);
+                    button.Size = new Size(cellSize, cellSize);
+                    button.BackColor = Color.White;
+                    if (j == 0 || i == 0)
+                    {
+                        button.BackColor = Color.Gray;
+                        if (i == 0 && j > 0)
+                            button.Text = alphabet[j - 1].ToString();
+                        if (j == 0 && i > 0)
+                            button.Text = i.ToString();
+                    }
+                    else
+                    {
+                        button.Click += new EventHandler(ConfigureShips);
+                    }
+                    myButtons[i, j] = button;
+                    this.Controls.Add(button);
+                }
+            }
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    myMap[i, j] = 0;
+                    enemyMap[i, j] = 0;
+
+                    Button button = new Button();
+                    button.Location = new Point(320 + j * cellSize, i * cellSize);
+                    button.Size = new Size(cellSize, cellSize);
+                    button.BackColor = Color.White;
+                    if (j == 0 || i == 0)
+                    {
+                        button.BackColor = Color.Gray;
+                        if (i == 0 && j > 0)
+                            button.Text = alphabet[j - 1].ToString();
+                        if (j == 0 && i > 0)
+                            button.Text = i.ToString();
+                    }
+                    else
+                    {
+                        button.Click += new EventHandler(PlayerShoot);
+                    }
+                    enemyButtons[i, j] = button;
+                    this.Controls.Add(button);
+                }
+            }
+            Label map1 = new Label();
+            map1.Text = "User map";
+            map1.Location = new Point(mapSize * cellSize / 2, mapSize * cellSize + 10);
+            this.Controls.Add(map1);
+
+            Label map2 = new Label();
+            map2.Text = "Enemy map";
+            map2.Location = new Point(350 + mapSize * cellSize / 2, mapSize * cellSize + 10);
+            this.Controls.Add(map2);
+
+            Button startButton = new Button();
+            startButton.Text = "Start";
+            startButton.Click += new EventHandler(Start);
+            startButton.Location = new Point(0, mapSize * cellSize + 20);
+            this.Controls.Add(startButton);
+        }
+
+        public void Start(object sender, EventArgs e)
+        {
+            isPlaying = true;
+
+        }
+
+        public bool CheckIfMapIsNotEmpty()
+        {
+            bool isEmpty1 = true;
+            bool isEmpty2 = true;
+            for (int i = 1; i < mapSize; i++)
+            {
+                for (int j = 1; j < mapSize; j++)
+                {
+                    if (myMap[i, j] != 0)
+                        isEmpty1 = false;
+                    if (enemyMap[i, j] != 0)
+                        isEmpty2 = false;
+                }
+            }
+            if (isEmpty1)
+            {
+                return false; // Гравець програв
+            }
+            else if (isEmpty2)
+            {
+                return true; // Гравець переміг
+            }
+            else
+            {
+                return true; // Гра триває
+            }
+        }
+
+        public void ConfigureShips(object sender, EventArgs e)
+        {
+            Button pressedButton = sender as Button;
+            if (!isPlaying)
+            {
+                if (myMap[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] == 0)
+                {
+                    pressedButton.BackColor = Color.Brown;
+                    myMap[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] = 1;
+                }
+                else
+                {
+                    pressedButton.BackColor = Color.White;
+                    myMap[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] = 0;
+                }
+            }
+        }
+
+        public void PlayerShoot(object sender, EventArgs e)
+        {
+            Button pressedButton = sender as Button;
+            if (pressedButton.BackColor == Color.White) // Дозволити стріляти тільки в білі клітинки
+            {
+                bool playerTurn = Shoot(enemyMap, pressedButton);
+                if (!playerTurn)
+                    bot.Shoot();
+
+                if (!CheckIfMapIsNotEmpty())
+                {
+                    this.Controls.Clear();
+                    Init();
+                    if (isPlaying)
+                        ShowWinMessage();
+                    else
+                        ShowLoseMessage();
+                }
+                else if (playerTurn && !CheckIfShipsLeft(enemyMap))
+                {
+                    this.Controls.Clear();
+                    Init();
+                    ShowWinMessage();
+                }
+            }
+        }
+
+        private bool CheckIfShipsLeft(int[,] map)
+        {
+            for (int i = 1; i < mapSize; i++)
+            {
+                for (int j = 1; j < mapSize; j++)
+                {
+                    if (map[i, j] != 0)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Shoot(int[,] map, Button pressedButton)
+        {
+            bool hit = false;
+            if (isPlaying)
+            {
+                int delta = 0;
+                if (pressedButton.Location.X > 320)
+                    delta = 320;
+                if (map[pressedButton.Location.Y / cellSize, (pressedButton.Location.X - delta) / cellSize] != 0)
+                {
+                    hit = true;
+                    map[pressedButton.Location.Y / cellSize, (pressedButton.Location.X - delta) / cellSize] = 0;
+                    pressedButton.BackColor = Color.Red;
+                    pressedButton.Text = "X";
+                }
+                else
+                {
+                    hit = false;
+
+                    pressedButton.BackColor = Color.Blue;
+                }
+            }
+            return hit;
+        }
+    }
+}
